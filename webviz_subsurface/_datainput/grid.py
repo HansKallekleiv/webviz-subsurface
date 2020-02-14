069142008157e970a89875e4f148cf76f2e4e379
Hans Kallekleiv <hakal@statoil.com>
2020-02-14 13:29:51 +0100

    Use functools.lru_cache for pyswig objects

--- webviz_subsurface/_datainput/grid.py
+++ webviz_subsurface/_datainput/grid.py
@@ -1,12 +1,14 @@
+import functools
+
 import xtgeo
-from webviz_config.common_cache import CACHE
 
-
-@CACHE.memoize(timeout=CACHE.TIMEOUT)
+# pylint: disable=no-member
+@functools.lru_cache()
 def load_grid(gridpath):
     return xtgeo.grid_from_file(gridpath)
 
 
-@CACHE.memoize(timeout=CACHE.TIMEOUT)
+# pylint: disable=no-member
+@functools.lru_cache()
 def load_grid_parameter(grid, gridparameterpath):
     return xtgeo.gridproperty_from_file(gridparameterpath, grid=grid)
