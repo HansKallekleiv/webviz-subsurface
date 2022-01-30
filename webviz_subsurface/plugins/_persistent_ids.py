462d11df11e50aa94c7061b2b99c8a768dc71f4b
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2022-01-30 13:45:45 +0100

    ids

--- webviz_subsurface/plugins/_persistent_ids.py
+++ webviz_subsurface/plugins/_persistent_ids.py
@@ -7,20 +7,24 @@ from webviz_config import WebvizPluginABC
 
 
 class PersistentIds(WebvizPluginABC):
+    COUNTER: int = 0
+
     def __init__(
         self,
     ):
+        PersistentIds.COUNTER += 1
 
         super().__init__()
 
     def persistent_uuid(self, str):
-        return f"{__name__}-{str}"
+        return f"{type(self).__name__}-{PersistentIds.COUNTER}-{str}"
 
     @property
     def layout(self) -> html.Div:
         return html.Div(
             [
                 html.Label(f"Persistent plugin id: {self.persistent_uuid('label')}"),
+                html.Br(),
                 html.Label(f"Generated plugin id: {self.uuid('label')}"),
             ]
         )
